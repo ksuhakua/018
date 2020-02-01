d54605b288e8004f80f457fe4cd3cd8c70ab0bee
ksuhak <ksuhak@Ksuhaks-MacBook-Pro.local>
2020-02-01 14:08:11 +0200

    done backup.sh

--- hometask_2/backup.sh
+++ hometask_2/backup.sh
@@ -4,11 +4,8 @@ date=$(date +%Y.%m.%d)
 echo "Please enter source directory:"
 read source_dir
 echo "Please enter backup directory:"
-read source_dir
-sudo mkdir $backup_dir/$date/
-for file in $source_dir -mtime -1; do
-       cp -pf {} $backup_dir/$date/ \;
-done
+read backup_dir
+mkdir -p $HOME/"$backup_dir"/"$date"
+find $HOME/$source_dir/ -mtime -1 -exec cp  {} $HOME/$backup_dir/$date/ \;
 
-
-#find $source_dir -mtime -1 -exec mkdir $backup_dir/$date/ & cp -pf {} $backup_dir/$date/ \;
+#/Documents/PE
