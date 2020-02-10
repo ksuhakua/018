363907156bd4d5847224669e2eeca03c4f563491
ksuhak <okovd@macmini5542.softservecom.com>
2020-02-10 16:18:21 +0200

    updated 3

---
+++ hometask_3/script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt install mysql-server -y
+sudo apt install mysql-client-core-5.7 -y
+
+sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456'"
+sudo mysql -uroot -p'123456' -e "FLUSH PRIVILEGES"
+mysql -uroot -p123456 <<MYSQL_SCRIPT
+CREATE DATABASE CONFLUENCE;
+CREATE USER 'root'@'%' IDENTIFIED BY '123456';
+GRANT ALL PRIVILEGES ON $"CONFLUENCE".* TO 'root'@'localhost';
+GRANT ALL PRIVILEGES ON $"CONFLUENCE".* TO 'root'@'%';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
+#sudo mysql -e "CREATE USER 'root'@'localhost' IDENTIFIED BY '123456'"
+#sudo mysql -uroot -p'123456' -e "CREATE DATABASE CONFLUENCE"
+#sudo mysql -uroot -p'123456' -e "GRANT ALL PRIVILEGES ON CONFLUENCE.* TO 'CONFLUENCE'@'localhost' IDENTIFIED BY '123456'"
+#sudo mysql -uroot -p'123456' -e "FLUSH PRIVILEGES"
+
+#drop user 'root'@'localhost';
+#GRANT ALL PRIVILEGES ON $CONFLUENCE.* TO 'root'@'localhost';
+
+sudo chmod +x atlassian-confluence-7.3.1-x64.bin
+sudo ./atlassian-confluence-7.3.1-x64.bin
